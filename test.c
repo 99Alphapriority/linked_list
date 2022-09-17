7766cedb8bbba52ab4a936e5f450c4bbc9ab9d03
alphapriority <prajneets@gmail.com>
2022-09-18 01:55:39 +0530

    test.c and test.h file added

---
+++ test.c
@@ -0,0 +1,15 @@
+void run_all_test()
+{
+	test_size();
+	test_push_front();
+	test_pop_front();
+	test_push_back();
+	test_pop_back();
+	test_value_at();
+	test_search();
+	test_insert();
+	test_erase();
+	test_reverse();
+	test_insert_sorted();
+	test_check_sorted();
+}
---
+++ test.h
@@ -0,0 +1,28 @@
+/*Header file declaring all the testcases*/
+
+/*Function to execute all the TC*/
+void run_all_test();
+/*Function to return number of links in a linked list*/
+void test_size();
+/*Function to add a node at the beginning of a linkedlist*/
+void test_push_front();
+/*Function to remove first node from a linked list*/
+void test_pop_front();
+/*Function to add a node at the end of a linked list*/
+void test_push_back();
+/*Function to remove last node from a linked list*/
+void test_pop_back();
+/*Function to display value in a given node*/
+void test_value_at();
+/*Function to find a given value in the node and print node index*/
+void test_search();
+/*Function to insert a node at a given index*/
+void test_insert();
+/*Function to remove a link at the given index*/
+void test_erase();
+/*Function to reverse a linked list*/
+void test_reverse();
+/*Function to insert in a sorted linked list*/
+void test_insert_sorted();
+/*Function to check if the linked list is sorted*/
+void test_check_sorted();
